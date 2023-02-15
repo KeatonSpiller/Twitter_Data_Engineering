8159fc9268a4481bb744622726835bf291efbcc8
KeatonSpiller <KeatonSpiller@gmail.com>
2023-02-15 00:39:32 -0800

    Appends new tweets to previously downloaded files

--- src/parse/stage1.py
+++ src/parse/stage1.py
@@ -2,9 +2,8 @@
 ## Parse Data
 #       Summary Overview
 #   - Using Tweepy API to parse data from Twitter API
-#   - 200 chunks of tweets for 3600 tweets for each User in user_input
+#   - 200 chunks of tweets for 3200 tweets for each User in user_input
 #   - Includes some data conditioning and curating with regex
-
 
 # %% [markdown]
 ## Import Libraries
@@ -32,13 +31,12 @@ autentication_path = os.path.abspath('./user_input/authentication_tokens.csv')
 api = twitter_authentication(autentication_path)
 
 # %% [markdown]
-# # Load Twitter Usernames
-# * Unvarified user's are not a problem, no one user can have the same ID
-#     
+# # Load Twitter Usernames   
 # | Removed User's | reason | 
 # | ------------ | ------------- |
 # |DayTradeWarrior|account removed from site|
 # |elonmusk|privated account|
+# * No two users can have the same id
 
 # %%
 with open(os.path.normpath(os.getcwd() + './user_input/user_list.xlsx'), 'rb') as f:
@@ -50,13 +48,12 @@ user_df
 
 # %% [markdown]
 # ## Download Tweets
-#     * Download User tweets into csv spreadsheets 
-# 
-# - ( Tweepy limit of 3600 tweets per user )
-#     
+#     * Download User tweets into csv spreadsheets
+#     * 3200 limit, adds to previously downloaded
 # %%
 for group in groups:
     print(f"\n{group}:\n")
     users = list(user_df[group][user_df[group]!= ''])
     user_download(api, users, group)
     print(f"")
+# %%
--- src/tools/twitter_tools.py
+++ src/tools/twitter_tools.py
@@ -1,103 +1,209 @@
 # %% [markdown]
 # # Import Libraries
 
-import os,sys,tweepy,nltk,re,glob, pandas as pd,numpy as np, yfinance as yf, string
+import os,tweepy,re,glob, pandas as pd,numpy as np, yfinance as yf, string, datetime
 np.random.seed(0)
 
+def dataframe_astypes():
+    """_summary_
+    
+    cleanly access dataframe conversions
+    
+    Returns:
+        dictionary: column names and pandas dataframe conversions
+        
+        { 'id': 'int64',
+        'created_at': 'datetime64[ns, UTC]',
+        'favorite_count': 'int64',
+        'retweet_count': 'int64',
+        'url': 'object',
+        'text': 'object'}
+    """
+    return { 'id': 'int64',
+            'created_at': 'datetime64[ns, UTC]',
+            'favorite_count': 'int64',
+            'retweet_count': 'int64',
+            'url': 'object',
+            'text': 'object'}
+    
 def user_download_helper(api, userID, group):
     """_summary_
-    Tweepy api download usertimeline from twitter api limited to 3600 tweets
-    Waits ~ 15 when time limit is up
-    removed usernames, punctuation, website links and special characters
-    converted time zone from UCT to EST
+    
+    Tweepy API download limit of 200 per chunk, Twitter API limit of 3200
+    Looping over individual user using usertimeline to append maximum tweets
+    limits on time scraping data, will occasionally wait ~ 15 then continue
+    removed usernames, punctuation, website links and special charactersfrom text
+    converted time zone from UTC to EST
     _why_
-    Args:
-        api (_type_): _description_
-        userID (_type_): _description_
-        group (_type_): _description_
+    
+    Strip particular user data from Twitter
+    Args input:
+        api (tweepy class): Handles parsing Twitter API
+        userID (list of strings): twitter usernames
+        group (string): label of users in list
+    Args output:
+        csv file in data/{group}/{username}.csv
+        {'id': 'int64',
+        'created_at': 'datetime64[ns, 'US/Eastern']', # converted to match yahoo finance EST
+        'favorite_count': 'int64',
+        'retweet_count': 'int64',
+        'url': 'object',
+        'text': 'object'}
     """
-    
-    tweets = api.user_timeline(screen_name=userID, 
-                            # 200 is the maximum allowed count
-                            count=200,
-                            include_rts = False,
-                            trim_user = False,
-                            tweet_mode = 'extended'
-                            )
-    all_tweets = []
-    all_tweets.extend(tweets)
-    oldest_id = tweets[-1].id
-    while True:
-        tweets = api.user_timeline(screen_name=userID, 
-                            # 200 is the maximum allowed count
-                            count=200,
-                            include_rts = False,
-                            max_id = oldest_id - 1,
-                            trim_user = False,
-                            tweet_mode = 'extended'
-                            )
-        if len(tweets) == 0:
-            break
-        oldest_id = tweets[-1].id
-        all_tweets.extend(tweets)
-        # print('N of tweets downloaded till now {}'.format(len(all_tweets)))
-        
-    regex = "(@[A-Za-z0-9]+)|(\w+:\/\/\S+)"
-    outtweets = []
-    for idx,tweet in enumerate(all_tweets):
-        # encode decode
-        txt = tweet.full_text
-        txt = txt.encode("utf-8").decode("utf-8")
-        # remove @ and website links
-        txt = ' '.join(re.sub(regex, " ", txt).split())
-        # remove punctuation
-        txt = re.sub(f"[{re.escape(string.punctuation)}]", "", txt)
-        # remove non characters
-        txt = re.sub(f"([^A-Za-z0-9\s]+)", "", txt)
-        # store as a string
-        txt = " ".join(txt.split())
-        tweet_list = [
-        tweet.id_str,
-        tweet.created_at,
-        tweet.favorite_count, 
-        tweet.retweet_count,
-        'https://twitter.com/i/web/status/' + tweet.id_str,
-        txt 
-        ]
-        outtweets.append(tweet_list)
-    df_temp = pd.DataFrame(outtweets, columns=['id','created_at','favorite_count',\
-                                                'retweet_count','url','text'])
-    
-    # using dictionary to convert specific columns
-    convert_dict = {'id': 'int64',
-                    'created_at': 'datetime64[ns, UTC]',
-                    'favorite_count': 'int64',
-                    'retweet_count': 'int64',
-                    'url': 'object',
-                    'text': 'object'}
-    df_temp = df_temp.astype(convert_dict)
-    df_temp.created_at = df_temp.created_at.dt.tz_convert('US/Eastern')
+    # Check if there exists previous history of user and group
     
     path = f'./data/{group}'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    df_temp.to_csv(path +'/'+ userID +'_twitter.csv',index=False)  
+    if os.path.exists(path):
+        try:
+            df_history = pd.read_csv(path +'/'+ userID +'_twitter.csv', parse_dates=['created_at']).reset_index(drop=True)
+            oldest_id = df_history.id.min()
+            since_id = df_history.id.max()
+        except Exception:
+            print("folder created without previous csv file")
+    else:
+        oldest_id = None
+    all_tweets = []
+    
+    # If first time running
+    #################################################
+    if(oldest_id == None):
+        tweets = api.user_timeline(screen_name=userID, 
+                                count=200,
+                                include_rts = False,
+                                trim_user = False,
+                                tweet_mode = 'extended'
+                                )
+        all_tweets.extend(tweets)
+        
+        oldest_id = tweets[-1].id
+        while True :
+            tweets = api.user_timeline(screen_name=userID,
+                                # 200 is the maximum allowed count
+                                count=200, 
+                                include_rts = False,
+                                max_id = oldest_id - 1,
+                                trim_user = False,
+                                tweet_mode = 'extended'
+                                )
+            if len(tweets) == 0:
+                break
+            oldest_id = tweets[-1].id
+            all_tweets.extend(tweets)
+    #################################################
+    else:
+        # Start where we ended
+        tweets = api.user_timeline(screen_name=userID, 
+                                count=200,
+                                include_rts = False,
+                                since_id = since_id,
+                                trim_user = False,
+                                tweet_mode = 'extended'
+                                )
+        # if we haven't downloaded today
+        if( len(tweets) != 0 ):
+            all_tweets.extend(tweets)
+            oldest_id = tweets[-1].id
+            since_id = tweets[0].id
+             
+            while True :
+                tweets = api.user_timeline(screen_name=userID,
+                                        # 200 is the maximum allowed count
+                                        count=200, 
+                                        include_rts = False,
+                                        since_id = since_id,
+                                        max_id = oldest_id - 1,
+                                        trim_user = False,
+                                        tweet_mode = 'extended'
+                                        )
+                if len(tweets) == 0:
+                    break
+                oldest_id = tweets[-1].id
+                all_tweets.extend(tweets)
+    #################################################
+    if(len(all_tweets) != 0):
+        regex = "(@[A-Za-z0-9]+)|(\w+:\/\/\S+)"
+        outtweets = []
+        for tweet in all_tweets:
+            # encode decode
+            txt = tweet.full_text
+            txt = txt.encode("utf-8").decode("utf-8")
+            # remove @ and website links
+            txt = ' '.join(re.sub(regex, " ", txt).split())
+            # remove punctuation
+            txt = re.sub(f"[{re.escape(string.punctuation)}]", "", txt)
+            # remove non characters
+            txt = re.sub(f"([^A-Za-z0-9\s]+)", "", txt)
+            # store as a string
+            txt = " ".join(txt.split())
+            # Pull data from the tweet
+            tweet_list = [
+                tweet.id_str,
+                tweet.created_at,
+                tweet.favorite_count, 
+                tweet.retweet_count,
+                'https://twitter.com/i/web/status/' + tweet.id_str,
+                txt 
+            ]
+            outtweets.append(tweet_list)
+        df_temp = pd.DataFrame(outtweets, columns=['id','created_at','favorite_count',\
+                                                    'retweet_count','url','text'])
+        
+        # using dictionary to convert specific columns
+        df_temp = df_temp.astype(dataframe_astypes())
+        # Convert UTC to US/Eastern
+        df_temp.created_at = df_temp.created_at.dt.tz_convert('US/Eastern')
+        
+        # If no previous history then create file else
+        if not os.path.exists(path):
+            os.makedirs(path)
+            df_temp.to_csv(path +'/'+ userID +'_twitter.csv',index=False)
+        else:
+            df_merge = pd.concat([df_temp, df_history],
+                                axis = 0,
+                                join = 'outer',
+                                names=['id',
+                                        'created_at',
+                                        'user',
+                                        'favorite_count',
+                                        'retweet_count',
+                                        'url',
+                                        'text'],
+                                ignore_index=True)
+            print(len(df_merge.id))
+            df_merge.to_csv(path +'/'+ userID +'_twitter.csv',index=False)
+    #################################################
+    else:  
+        print(f"no recent tweets")  
+             
     
 def user_download(api, user_list, group):
     """_summary_
+    
     Download users within Excel list of usernames and save in a csv under data
     _why_
+    
+    runs user_download_helper for each user to download tweets 
     Args:
-        api (_type_): _description_
-        user_list (_type_): _description_
-        group (_type_): _description_
+        Args input:
+        api (tweepy class): Handles parsing Twitter API
+        userID (list): twitter usernames
+        group (string): label of users in list
+    Args output:
+        csv file in data/{group}/{username}.csv
+        {'id': 'int64',
+        'created_at': 'datetime64[ns, 'US/Eastern']', # converted to match yahoo finance EST
+        'favorite_count': 'int64',
+        'retweet_count': 'int64',
+        'url': 'object',
+        'text': 'object'}
     """
-    try:
-        for userID in user_list:
-            user_download_helper(api, userID, group)
+    
+    for userID in user_list:
+        try:
             print(userID, end=' ')
-    except Exception:
-        print(f"Invalid user: {userID}")
+            user_download_helper(api, userID, group)
+        except Exception as e:
+            print (str(e))
 
 def merge_files(group, display):
     """_summary_
@@ -109,24 +215,18 @@ def merge_files(group, display):
     """
     csv_files = glob.glob(os.path.join('./data'+"/"+group, "*.csv"))
     df = pd.DataFrame()
-    convert_dict = {'id': 'int64',
-                        'user':'object',
-                        'favorite_count': 'int64',
-                        'retweet_count': 'int64',
-                        'url': 'object',
-                        'text': 'object'}
     for f in csv_files:
         # read the csv file
         df_temp = pd.read_csv(f)
         user_row = f.split("\\")[-1].split(".")[0]
         df_temp.insert(2, 'user', user_row)
-        df_temp = df_temp.astype(convert_dict)
+        df_temp = df_temp.astype(dataframe_astypes())
         if( display > 0):
             display(df_temp.iloc[0:display])
             print(df_temp.shape)
         # Merging columns of groups
         df = pd.concat([df_temp,df], axis = 0, join = 'outer', names=['id','created_at','user','favorite_count',\
-                                                                      'retweet_count','url','text']).astype(convert_dict)
+                                                                      'retweet_count','url','text']).astype(dataframe_astypes())
         
     print(f"size of merged data sets of {group}: {df.shape}")
     
@@ -153,23 +253,16 @@ def merge_all(group, display):
     """
     csv_files = glob.glob(os.path.join('./data'+"/"+group, "*.csv"))
     df = pd.DataFrame()
-    convert_dict = {'id': 'int64',
-                        'user':'object',
-                        'favorite_count': 'int64',
-                        'retweet_count': 'int64',
-                        'url': 'object',
-                        'text': 'object'}
     for f in csv_files:
         # read the csv file
-        df_temp = pd.read_csv(f)
-        df_temp = df_temp.astype(convert_dict)
+        df_temp = pd.read_csv(f).astype(dataframe_astypes())
         # using dictionary to convert specific columns
         if( display > 0):
             display(df_temp.iloc[0:display])
             print(df_temp.shape)
         # Merging columns of everything
         df = pd.concat([df_temp,df], axis = 0, join = 'outer',names=['id','created_at','user','favorite_count',\
-                                                                     'retweet_count','url','text']).astype(convert_dict)
+                                                                     'retweet_count','url','text']).astype(dataframe_astypes())
          
     print(f"size of merged data sets of {group.split('/')[1]}: {df.shape}")
     
@@ -291,8 +384,8 @@ def download_todays_test(ticker_df, df_normalized, df_original):
     current_price = yf.download(stock_str, period='1d', interval = '1m', progress=False)['Close']
     current_price = current_price.loc[[str(current_price.index.max())]].reset_index('Datetime').rename(columns= column_names)
     
-    convert_dict = dict(zip(ticker_df.ticker_label, ['float64']*len(ticker_df.ticker_label)))
-    current_price = current_price.astype(convert_dict)
+    convert_list = dict(zip(ticker_df.ticker_label, ['float64']*len(ticker_df.ticker_label)))
+    current_price = current_price.astype(convert_list)
     current_price.date = current_price.date.dt.date
     current_price = current_price.astype({'date':'datetime64[ns]'}).set_index('date')
     
